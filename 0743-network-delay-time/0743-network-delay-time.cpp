9c407da7699754086d48d694e70705f4025a5854
Karan <karan5kumarrr@gmail.com>
2026-07-02 22:26:21 +0530

    Time: 91 ms (33.69%), Space: 47.9 MB (25.73%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,53 +1,39 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-
-        vector<vector<pair<int,int>>> adj(n + 1);
-
-        for(auto &it : times){
+        vector<vector<pair<int, int>>> adj(n+1);
+        for (auto it: times){
             adj[it[0]].push_back({it[1], it[2]});
         }
-
-        vector<int> dist(n + 1, 1e9);
-
-        priority_queue<
-            pair<int,int>,
-            vector<pair<int,int>>,
-            greater<pair<int,int>>
-        > pq;
-
-        dist[k] = 0;
+        vector<int> time(n+1, 1e9);
+        time[k] = 0;
+        priority_queue<pair<int, int>,
+                    vector<pair<int, int>>,
+                    greater<pair<int, int>>> pq;
         pq.push({0, k});
-
         while(!pq.empty()){
-
-            int tm = pq.top().first;
             int node = pq.top().second;
+            int tm = pq.top().first;
             pq.pop();
-
-            for(auto it : adj[node]){
-
-                int adjNode = it.first;
-                int wt = it.second;
-
-                if(tm + wt < dist[adjNode]){
-
-                    dist[adjNode] = tm + wt;
-                    pq.push({dist[adjNode], adjNode});
+            for (auto it: adj[node]){
+                int val = it.first;
+                int etm = it.second;
+                if (tm + etm < time[val]){
+                    time[val] = tm+etm;
+                    pq.push({time[val], val});
                 }
             }
         }
-
         int ans = 0;
-
         for(int i = 1; i <= n; i++){
 
-            if(dist[i] == 1e9)
+            if(time[i] == 1e9)
                 return -1;
 
-            ans = max(ans, dist[i]);
+            ans = max(ans, time[i]);
         }
 
         return ans;
     }
+
 };
