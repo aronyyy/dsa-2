6b08f7ee1f38c0cac99205224336a3ebfbb44f84
Karan <karan5kumarrr@gmail.com>
2026-07-17 23:59:38 +0530

    Time: 3 ms (28.74%), Space: 10.5 MB (12.62%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
-    bool solve(string &s, int left, int right) {
-
-        if (left >= right)
-            return true;
-
-        // Skip non-alphanumeric characters
-        while (left < right && !isalnum(s[left]))
-            left++;
-
-        while (left < right && !isalnum(s[right]))
-            right--;
-
-        // Compare after converting to lowercase
-        if (tolower(s[left]) != tolower(s[right]))
-            return false;
-
-        return solve(s, left + 1, right - 1);
-    }
-
     bool isPalindrome(string s) {
-        return solve(s, 0, s.size() - 1);
+        vector<char> string;
+        for(auto it: s){
+            if (isalnum(it)){
+                string.push_back(tolower(it));
+            }
+        }
+        int si = 0;
+        int e = string.size() - 1;
+        while (si < e){
+            if (string[si++] != string[e--]) return false;
+        }
+        return true;
     }
 };
