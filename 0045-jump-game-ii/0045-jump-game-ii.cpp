7eba1e0ae710ded643dcd6fec685a71c7f392874
Karan <karan5kumarrr@gmail.com>
2026-07-26 09:41:19 +0530

    Time: 0 ms (100%), Space: 20.6 MB (50.39%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
-    int dfs(vector<int>& nums, vector<int>& dp, int i) {
-
-        if (i == 0)
-            return 0;
-
-        if (dp[i] != -1)
-            return dp[i];
-
-        int ans = INT_MAX;
-
-        for (int j = 0; j < i; j++) {
-            if (j + nums[j] >= i) {
-                ans = min(ans, 1 + dfs(nums, dp, j));
-            }
-        }
-
-        return dp[i] = ans;
-    }
-
     int jump(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n, -1);
-        return dfs(nums, dp, n - 1);
+        int jumps=0;
+        int l=0;
+        int r=0;
+        while(r<nums.size()-1){
+            int farthest = 0;
+            for (int i=l; i<=r; i++){
+                farthest = max(farthest, nums[i]+i);
+            }
+            l = r+1;
+            r = farthest;
+            jumps++;
+        }
+        return jumps;
     }
 };
