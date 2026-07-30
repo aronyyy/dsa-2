b5f6a98fe271caa855314298025440210f4e5233
Karan <karan5kumarrr@gmail.com>
2026-07-30 19:24:16 +0530

    Time: 30 ms (33.36%), Space: 24.2 MB (21.63%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -5,7 +5,6 @@ private:
         int dcol[] = {0,1,0,-1};
         queue<pair<int, int>> q;
         q.push({row, col});
-        vis[row][col] = 1;
         while(!q.empty()){
             int row = q.front().first;
             int col = q.front().second;
@@ -13,11 +12,13 @@ private:
             for(int i=0; i<4; i++){
                 int nrow = row + drow[i];
                 int ncol = col + dcol[i];
+                
                 if (nrow >= 0 && nrow < grid.size()
                     && ncol >= 0 && ncol < grid[0].size()
                     && grid[nrow][ncol] == '1' && !vis[nrow][ncol]){
-                        q.push({nrow, ncol});
                         vis[nrow][ncol] = 1;
+                        q.push({nrow, ncol});
+                        
                     }
             }
         }
