54de0cb0a99139fa88b92a647f2ebb867cb83a1e
Karan <karan5kumarrr@gmail.com>
2026-07-30 19:13:01 +0530

    Time: 26 ms (62.88%), Space: 24.4 MB (17.37%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,18 +1,27 @@
 class Solution {
 private:
-    void dsf(int row, int col, vector<vector<char>>& grid, vector<vector<int>>& vis){
+    void bfs(int row, int col, vector<vector<char>>& grid, vector<vector<int>>& vis){
         int drow[] = {-1,0,1,0};
         int dcol[] = {0,1,0,-1};
+        queue<pair<int, int>> q;
+        q.push({row, col});
         vis[row][col] = 1;
-        for (int i=0; i<4; i++){
-            int nrow = row + drow[i];
-            int ncol = col + dcol[i];
-            if (nrow>=0 && nrow<grid.size()
-                && ncol>=0 && ncol<grid[0].size()
-                && grid[nrow][ncol]=='1' && !vis[nrow][ncol]){
-                    dsf(nrow, ncol, grid, vis);
-                }
+        while(!q.empty()){
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+            for(int i=0; i<4; i++){
+                int nrow = row + drow[i];
+                int ncol = col + dcol[i];
+                if (nrow >= 0 && nrow < grid.size()
+                    && ncol >= 0 && ncol < grid[0].size()
+                    && grid[nrow][ncol] == '1' && !vis[nrow][ncol]){
+                        q.push({nrow, ncol});
+                        vis[nrow][ncol] = 1;
+                    }
+            }
         }
+        
 
     }
 public:
@@ -25,7 +34,7 @@ public:
             for (int j=0; j<c; j++){
                 if (grid[i][j] == '1' && !vis[i][j]){
                     cnt++;
-                    dsf(i, j, grid, vis);
+                    bfs(i, j, grid, vis);
                 }
             }
         }
