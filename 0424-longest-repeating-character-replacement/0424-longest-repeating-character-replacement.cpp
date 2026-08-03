2be55aff4d19971654042f4ed77aca2be1adfd69
Karan <karan5kumarrr@gmail.com>
2026-08-03 12:16:04 +0530

    Time: 10 ms (31.91%), Space: 11.1 MB (19.59%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -2,30 +2,21 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         int n = s.size();
-        int maxL = 0;
         int l=0;
         int r=0;
-        int maxF = 0;
-        unordered_map<char, int> mp;
-        while(r < n){
-            mp[s[r]]++;
-            maxF = max(maxF, mp[s[r]]);
-            int changes = r - l + 1 - maxF;
-
-            while(changes > k){
-                mp[s[l]]--;
-                maxF = 0;
-                for(auto it: mp){
-                    maxF = max(maxF, it.second);
-                }
+        unordered_map<char, int>mpp;
+        int maxF=0;
+        int ans = 0;
+        while(r<n){
+            mpp[s[r]]++;
+            maxF = max(maxF, mpp[s[r]]);
+            while(maxF < r-l+1-k){
+                mpp[s[l]]--;
                 l++;
-                changes = r - l + 1 - maxF;
             }
-
-            maxL = max(maxL, r-l+1);
             r++;
+            ans = max(r-l+1, ans);
         }
-        return maxL;
+        return ans-1;
     }
-
 };
