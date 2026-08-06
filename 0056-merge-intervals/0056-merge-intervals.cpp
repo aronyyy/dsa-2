8cabdc62ba2b92bc8e4a24aecb3dd1fdf3da5dab
Karan <karan5kumarrr@gmail.com>
2026-08-06 19:34:12 +0530

    Time: 3 ms (88.78%), Space: 23.7 MB (82.59%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     static bool cmp(vector<int>& a, vector<int>& b){
-        return a[0] > b[0];
+        return a[0] < b[0];
     }
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        sort(intervals.begin(), intervals.end());
+        sort(intervals.begin(), intervals.end(), cmp);
 
         vector<vector<int>> ans;
 
