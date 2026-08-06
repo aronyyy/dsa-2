6a8f0d46fa97226bb95b31ac5c70e5b93b8c9f6a
Karan <karan5kumarrr@gmail.com>
2026-08-06 19:33:52 +0530

    Time: 7 ms (38.03%), Space: 23.8 MB (82.59%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    static bool cmp(vector<int>& a, vector<int>& b){
+        return a[0] > b[0];
+    }
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+
+        vector<vector<int>> ans;
+
+        ans.push_back(intervals[0]);
+
+        for (int i = 1; i < intervals.size(); i++) {
+
+            // Overlapping intervals
+            if (intervals[i][0] <= ans.back()[1]) {
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
+            }
+            // Non-overlapping interval
+            else {
+                ans.push_back(intervals[i]);
+            }
+        }
+
+        return ans;
+    }
+};
